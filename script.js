42153a265ab0ae9d6e114d2309d55d939b659a65
BssEric <160087132+BssEric@users.noreply.github.com>
2024-02-22 19:54:47 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("botaoInfo").onclick = function() {
+ var info = document.getElementById("informacao");
+if (info.style.display === "none"){
+    info.style.display = "block";
+} else {
+    info.style.display = "none";
+}
+}
